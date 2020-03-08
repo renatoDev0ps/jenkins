1caeacde3aa725d851553976656a8fb5f606e3b9
infordigi <renato.nascimento2009@gmail.com>
2020-03-08 14:09:17 -0300

    Criação da versão 2.0.0

--- recursos/v.2.0.0/setup-and-run.sh
+++ recursos/v.2.0.0/setup-and-run.sh
@@ -7,8 +7,31 @@ image_name=devops-jenkins
 image_version=2.0.0
 container_name=md-jenkins
 
-docker pull jenkins:2.222-jdk11
+docker pull jenkins:2.223-jdk11
 
 if [ ! -d downloads ]; then
   mkdir downloads
-  curl -o downloads/jdk
+  curl -o downloads/apache-maven-3.6.3-bin.tar.gz http://mirror.vorboss.net/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+fi
+
+docker stop ${container_name}
+
+docker build --no-cache -t ${dockerhub_user}/${image_name}:${image_version} .
+
+if [ ! -d m2deps ]; then
+  mkdir m2deps
+fi
+if [ -d jobs ]; then
+  rm -rf jobs
+fi
+if [ ! -d jobs ]; then
+  mkdir jobs
+fi
+
+docker run -p ${jenkins_port}:8080 -d \
+    -v `pwd`/downloads:/var/jenkins_home/downloads \
+    -v `pwd`/jobs:/var/jenkins_home/jobs/ \
+    -v `pwd`/m2deps:/var/jenkins_home/.m2/repository/ \
+    -v $HOME/.ssh:/var/jenkins_home/.ssh/ \
+    --rm --name ${container_name} \
+    ${dockerhub_user}/${image_name}:${image_version}
