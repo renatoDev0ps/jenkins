16f0ba170fc3472704d63b114c5195b497365d22
renatoDev0ps <renatodevops@gmail.com>
2020-03-13 17:04:30 -0300

    atualização da versão 2.0.1

--- recursos/v.2.0.1/setup-and-run.sh
+++ recursos/v.2.0.1/setup-and-run.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
 
-dockerhub_user=rodrigogrohl
+dockerhub_user=renatodevops
 
 jenkins_port=8080
-image_name=missao-devops-jenkins
-image_version=2.0.0
+image_name=devops-jenkins
+image_version=2.0.1
 container_name=md-jenkins
 
-docker pull jenkins:2.112
+docker pull jenkins:2.219
 
 if [ ! -d downloads ]; then
     mkdir downloads
-    curl -o downloads/jdk-8u144-linux-x64.tar.gz http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-8u144-linux-x64.tar.gz
-    curl -o downloads/jdk-7u80-linux-x64.tar.gz http://ftp.osuosl.org/pub/funtoo/distfiles/oracle-java/jdk-7u80-linux-x64.tar.gz
-    curl -o downloads/apache-maven-3.5.2-bin.tar.gz http://mirror.vorboss.net/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+    curl -o downloads/jdk-8u212-linux-x64.tar.gz https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-x64.tar.gz
+    curl -o downloads/jdk-7u80-linux-x64.tar.gz https://github.com/frekele/oracle-java/releases/download/7u80-b15/jdk-7u80-linux-x64.tar.gz
+    curl -o downloads/apache-maven-3.6.3-bin.tar.gz http://mirror.vorboss.net/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
 fi
 
 docker stop ${container_name}
@@ -30,7 +30,7 @@ if [ ! -d jobs ]; then
     mkdir jobs
 fi
 
-docker run -p ${jenkins_port}:8080 \
+docker run -p ${jenkins_port}:8080 -d \
     -v `pwd`/downloads:/var/jenkins_home/downloads \
     -v `pwd`/jobs:/var/jenkins_home/jobs/ \
     -v `pwd`/m2deps:/var/jenkins_home/.m2/repository/ \
